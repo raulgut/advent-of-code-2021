938df18c03f4fc7a5be5382db39c895894ae08b2
Raúl Gutiérrez <rgutierrez11@gmail.com>
2021-12-05 20:38:37 +0100

    Day 5

---
+++ src/five-two.hs
@@ -0,0 +1,58 @@
+-----------------------------------------------------------------------------
+-- | Second part of Day 5
+--
+-- Precondition: we have only horizontal, vertical or diagonal lines
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec (Parser)
+import Text.Parsec.Char (digit, string, char)
+import Text.Parsec.Combinator (many1)
+import Text.Parsec.Prim (parse)
+import Data.Map as M (Map, empty, lookup, insert, filter, size)
+
+-----------------------------------------------------------------------------
+-- Functions
+-----------------------------------------------------------------------------
+
+main = do
+  args <- getArgs
+  input <- readFile . head $ args
+  let vents = (map extractVents . lines $ input) :: [((Int,Int), (Int, Int))]
+  putStrLn . show . size . M.filter (>1) . foldl processVent empty $ vents
+
+-- | Parses vent coordinates
+extractVents :: String -> ((Int,Int), (Int, Int))
+extractVents str
+  = case (parse ventParser "" str) of
+      Left err -> error . show $ err
+      Right coord -> coord
+
+-- | parses vent coordinates
+ventParser :: Parser ((Int,Int), (Int, Int))
+ventParser =
+ do x1 <- many1 digit
+    char ',' 
+    y1 <- many1 digit
+    string " -> " 
+    x2 <- many1 digit
+    char ',' 
+    y2 <- many1 digit
+    return $ ((read x1, read y1),(read x2,read y2))
+
+-- | for each vent, we add 1 to its coordinates
+processVent :: Map (Int,Int) Int -> ((Int, Int),(Int, Int)) -> Map (Int,Int) Int
+processVent ventMap ((x1,y1),(x2,y2)) | x1 == x2 && y1 < y2 = foldl updateVent ventMap [(x1,y) | y <- [y1..y2]] 
+processVent ventMap ((x1,y1),(x2,y2)) | x1 == x2 = foldl updateVent ventMap [(x1,y) | y <- [y2..y1]]
+processVent ventMap ((x1,y1),(x2,y2)) | y1 == y2 && x1 < x2 = foldl updateVent ventMap [(x,y1) | x <- [x1..x2]]
+processVent ventMap ((x1,y1),(x2,y2)) | y1 == y2 = foldl updateVent ventMap [(x,y1) | x <- [x2..x1]]
+processVent ventMap ((x1,y1),(x2,y2)) | x1 < x2 && y1 < y2 = foldl updateVent ventMap [(x1 + i, y1 + i) | i <- [0..(x2 - x1)]]
+processVent ventMap ((x1,y1),(x2,y2)) | x1 < x2 = foldl updateVent ventMap [(x1 + i, y1 - i) | i <- [0..(x2 - x1)]]
+processVent ventMap ((x1,y1),(x2,y2)) | y1 < y2 = foldl updateVent ventMap [(x1 - i, y1 + i) | i <- [0..(x1 - x2)]]
+processVent ventMap ((x1,y1),(x2,y2)) = foldl updateVent ventMap [(x1 - i, y1 - i) | i <- [0..(x1 - x2)]]
+
+-- | given a coordinate, increase its occurrence
+updateVent :: Map (Int,Int) Int -> (Int, Int) -> Map (Int,Int) Int
+updateVent ventMap (x,y) = case M.lookup (x,y) ventMap of
+                             Nothing -> M.insert (x,y) 1 ventMap
+                             Just value -> M.insert (x,y) (value + 1) ventMap
