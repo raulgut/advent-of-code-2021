095e23e722ababb4b3f7c8b6cad1aed55ef97db0
Raúl Gutiérrez <rgutierrez11@gmail.com>
2021-12-20 00:38:53 +0100

    Day 17

---
+++ src/seventeen-one.hs
@@ -0,0 +1,55 @@
+-----------------------------------------------------------------------------
+-- | First part of Day 17
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec (Parser, try, string)
+import Text.Parsec.Char (digit, char)
+import Text.Parsec.Combinator (many1)
+import Text.Parsec.Prim (parse, (<|>))
+import Control.Monad (liftM, (>>=))
+import Data.List (maximum, minimum)
+
+main = do
+  args <- getArgs
+  input <- readFile . head $ args
+  let target = (extractTA $ input) :: ((Int,Int),(Int,Int))
+  putStrLn . show . (\x -> sum [0..x]) . maximum . snd . foldl1 getMax . processValidYs 1 (snd . fst $ target) . snd $ target
+
+-- | Parses the entry
+extractTA :: String -> ((Int,Int),(Int,Int))
+extractTA str
+  = case (parse targetParser "" str) of
+      Left err -> error . show $ err 
+      Right ta -> ta
+
+-- | Parses the target area
+targetParser :: Parser ((Int,Int),(Int,Int))
+targetParser 
+  = do { string "target area: x="
+       ; x0 <- ((try (char '-')) >> many1 digit >>= (return . ('-':))) <|> many1 digit
+       ; string ".." 
+       ; x1 <- ((try (char '-')) >> many1 digit >>= (return . ('-':))) <|> many1 digit
+       ; string ", y="
+       ; y0 <- ((try (char '-')) >> many1 digit >>= (return . ('-':))) <|> many1 digit
+       ; string ".."
+       ; y1 <- ((try (char '-')) >> many1 digit >>= (return . ('-':))) <|> many1 digit
+       ; return ((read x0,read x1),(read y0,read y1))
+       }
+
+-- | for each number of jumps i, calculate the possible Y values
+processValidYs :: Int -> Int -> (Int,Int) -> [(Int,[Int])]
+processValidYs i limit (minY,maxY) = let validYi = (i,[div distance i | value <- [minY..maxY], let distance = value + sum [0..(i - 1)], rem distance i == 0 ])
+                                     in if i > (limit * 2) then
+                                          []
+                                        else
+                                          validYi:processValidYs (i + 1) limit (minY,maxY)
+
+-- | get the maximum height
+getMax :: (Int,[Int]) -> (Int,[Int]) -> (Int,[Int]) 
+getMax (i,vs) (j,[]) = (i,vs)
+getMax (i,[]) (j,ws) = (j,ws)
+getMax (i,vs) (j,ws) = if maximum ws > maximum vs then 
+                         (j,ws)
+                       else 
+                         (i,vs) 
